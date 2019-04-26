f07fce43d90cb7ec90faba972da1face3fd9a518
Leo Xiong <39155356+XZLeo@users.noreply.github.com>
2019-04-26 11:19:20 +0800

    Update mainwindow.cpp
    source setenv.sh不要忘了source，否则会提示权限不足；和win上的运行不一样。

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -7,111 +8,41 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     cmd = new QProcess(this);
-    connect(cmd, SIGNAL(readyReadStandardOutput()),this,SLOT(on_readoutput()));
-    connect(cmd,SIGNAL(readyReadStandardOutput()),this, SLOT(on_readoutput()));
+    connect(cmd , SIGNAL(readyReadStandardOutput()) , this , SLOT(on_readoutput())); //readyReadStandardOutPut
+    connect(cmd , SIGNAL(readyReadStandardError()) , this , SLOT(on_readerror())); //SIGNAL宏指令
 
     cmd->start("bash");
     cmd->waitForStarted();
-    cmd->write("/home/leo/OpenDDS-3.13.2/bin/setenv.sh"+'\n');
-
+    cmd->write("source /home/leo/OpenDDS-3.13.2/setenv.sh\n");
 }
 
 MainWindow::~MainWindow()
 {
-    if(cmd)
-    {
-        cmd->terminate();
-        cmd->waitForFinished();
-    }
+        if(cmd)
+        {
+            cmd->terminate();
+            cmd->waitForFinished();
+        }
     delete ui;
 }
 
-void MainWindow::on_readoutput(){
+void MainWindow::on_readoutput()
+{
     ui->textEdit->append(cmd->readAllStandardOutput().data());
 }
 
-void MainWindow::on_readerror(){
-    QMessageBox::information(0,"Error",cmd->readAllStandardError().data());
-}
-
-void MainWindow::on_middleware_clicked()
+void MainWindow::on_readerror()
 {
-    ui->textEdit->clear();
-    cmd->write("/home/leo/OpenDDS-3.13.2/bin/DCPSInfoRepo"
-               "-ORBEndpoint iiop://localhost:12345"+'\n'); \\一定要加\n!
-    /*
-    QProcess process(this);
-    process.setProgram("/home/leo/OPenDDS-3.13.2/bin/DCPSInfoRepo");
-    QStringList argument;
-    argument<<"-ORBEndpoint iiop://localhost:12345";
-    process.setArguments(argument);
-    process.start();
-    process.waitForStarted();
-    QString temp = "DCPS Information Repository Starts";
-    ui->textEdit->setText(temp);*/
+    QMessageBox::information(0, "Error", cmd->readAllStandardError().data());
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 void MainWindow::on_pushButton_clicked()
 {
-    ui->textEdit->clear();
-    QProcess process(this);
-    process.setProgram("/home/leo/OPenDDS-3.13.2/bin/DCPSInfoRepo");
-    QStringList argument;
-    argument<<"-ORBEndpoint iiop://localhost:12345";
-    process.setArguments(argument);
-    process.start();
-    process.waitForStarted();
-    QString temp = "DCPS Information Repository Starts";
-    ui->textEdit->setText(temp);
-}
+    cmd->write(ui->lineEdit->text().toLocal8Bit() + '\n');  //读入lineedit的然后输入给写入cmd
+    // cmd->write("/home/leo/OpenDDS-3.13.2/bin"
+              //"/DCPSInfoRepo -ORBEndpoint iiop://localhost:12345\n"); //启动之后前端进程会阻塞（点了没反应）
+} //Qt里写命令最后一定要有\n相当于回车，不然不会执行命令！！！！
 
-void MainWindow::on_pushButton_2_clicked()
-{
-    QProcess process(this);
-    //process.setProgram("/home/leo/OpenDDS-3.13.2/Timer/publisher");
-    process.start("bash");
-    process.waitForStarted();
-    QString temp = "Timer Starts";
-    ui->textEdit->setText(temp);
-    process.waitForFinished();
-}
-
-
-
-void MainWindow::on_pushButton_3_clicked()
-{
-    QProcess process(this);
-    process.setProgram("/home/leo/OpenDDS-3.13.2/Timer/subscriber");
-    process.start();
-    process.waitForStarted();
-    process.waitForReadyRead();
-    QString temp = QString::fromLocal8Bit(process.readAllStandardOutput()); //关键是标准读入的数据是什么形式的
-    ui->textEdit->setText(temp); //
-}*/
-
+//很多库没有，要配置环境那个变量,用setenv.sh
